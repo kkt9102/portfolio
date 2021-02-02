1fcf6d7c630b63b2be35406e1f01523f1b2c517e
kkt9102 <kkt9102@gmail.com>
2021-02-02 22:51:10 +0900

    iframe 적용연습중

---
+++ portfolio/reserve/resource/js/footer.js
@@ -0,0 +1,6 @@
+// foot include
+$(document).ready(function(){
+
+    document.getElementById("footer").innerHTML='<object type="text/html" data="foot.html"></object>';
+
+})
---
+++ portfolio/reserve/resource/js/header.js
@@ -0,0 +1,6 @@
+// head include
+$(document).ready(function(){
+
+    document.getElementById("header").innerHTML='<object type="text/html" data="/part/head.html"></object>';
+
+})
