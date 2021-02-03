28d42673668313c8589fd6e8bb33a6934f3eb91a
kkt9102 <kkt9102@gmail.com>
2021-02-03 20:40:05 +0900

    html include 성공

---
+++ portfolio/reserve/resource/js/include.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+    document.getElementById("header").innerHTML='<object type="text/html" style="width:100%;" data="include/header.html"></object>';
+
+})
+
+$(document).ready(function(){
+
+    document.getElementById("footer").innerHTML='<object type="text/html" data="include/footer.html"></object>';
+
+})
