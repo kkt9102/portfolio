9cc534525674478b9acab513bf32a4ce86abce76
kkt9102 <kkt9102@gmail.com>
2021-02-14 21:52:42 +0900

    메뉴 1차해결

--- portfolio/reserve/resource/js/common.js
+++ portfolio/reserve/resource/js/common.js
@@ -30,32 +30,86 @@ function Coffee_item() {
         var $Item_index = $('#section-4 .section-4_con .menu-box-3 ul li').length;
         var $String = $Item_index.toString()[1];
     
-    
-        if ($Item_index.toString().length == 2 ){
-            if ($String == [3 && 7]) {
-                $Item.append('<li>test1</li><li>test1</li><li>test1</li>');
-                alert('3');
-            } else if ($String == [4 && 8]) {
-                $Item.append('<li>test1</li><li>test1</li>');
-                alert('2');
-            } else if ($String == [1 && 5 && 9]) {
-                $Item.append('<li>test1</li>');
-                alert('1');
-            } else if ($String == [0 && 2 && 6]) {
-                return;
-            }
+        if ($Item_index.toString().length == 2||$String == [1 && 5 && 9]) {
+            $Item.after(
+                '<li class="dummy">' +
+                    '<div></div>' +
+                '</li>'
+                );
+        } else if ($Item_index.toString().length == 2||$String == [3 && 7]) {
+            $Item.after(
+                '<li class="dummy">' +
+                    '<div></div>' +
+                '</li>' +
+                '<li class="dummy">' +
+                    '<div></div>' +
+                '</li>' +
+                '<li class="dummy">' +
+                    '<div></div>' +
+                '</li>'
+            );
+        } else if ($Item_index.toString().length == 2||$String == [4 && 8]) {
+            $Item.after(
+                '<li class="dummy">' +
+                    '<div></div>' +
+                '</li>' +
+                '<li class="dummy">' +
+                    '<div></div>' +
+                '</li>'
+            );
         } else {
-            if ($String == [3&&7]) {
-                $Item.append('<li>test1</li><li>test1</li><li>test1</li>');
-            } else if ($String == [4&&8]) {
-                $Item.append('<li>test1</li><li>test1</li>');
-            } else if ($String == [1&&5&&9]) {
-                $Item.append('<li>test1</li>');
-            } else if ($String == [0&&2&&6]) {
-                return;
-            }
-        };
+            return;
+        }
+    
+        // if ($Item_index.toString().length == 2 ){
+        //     if ($String == [3 && 7]) {
+        //         $Item.after('<li>test1</li><li>test1</li><li>test1</li>');
+        //     } else if ($String == [4 && 8]) {
+        //         $Item.after('<li>test1</li><li>test1</li>');
+        //     } else if ($String == [1 && 5 && 9]) {
+        //         $Item.after('<li>test1</li>');
+        //     } else if ($String == [0 && 2 && 6]) {
+        //         return;
+        //     };
+        // } else {
+        //     if ($String == [3&&7]) {
+        //         $Item.after('<li>test1</li><li>test1</li><li>test1</li>');
+        //     } else if ($String == [4&&8]) {
+        //         $Item.after('<li>test1</li><li>test1</li>');
+        //     } else if ($String == [1&&5&&9]) {
+        //         $Item.after('<li>test1</li>');
+        //     } else if ($String == [0&&2&&6]) {
+        //         return;
+        //     }
+        // };
 }
+// function Coffee_item(){
+//     var $Item_index = $('.menu-box-3 ul li').length;
+//     var $String = $Item_index.toString()[1];
+
+
+//     if ($Item_index.toString().length == 2 ){
+//         if ($String == [3&&7]) {
+//             $('.menu-box-3 ul li:last-child').after('<li>test1</li><li>test1</li><li>test1</li>');
+//         } else if ($String == [4&&8]) {
+//             $('.menu-box-3 ul li:last-child').after('<li>test1</li><li>test1</li>');
+//         } else if ($String == [1&&5&&9]) {
+//             $('.menu-box-3 ul li:last-child').after('<li>test1</li>');
+//         } else if ($String == [0&&2&&6]) {
+//             return;
+//         }
+//     } else {
+//         if ($String == [3&&7]) {
+//             $('.menu-box-3 ul li:last-child').after('<li>test1</li><li>test1</li><li>test1</li>');
+//         } else if ($String == [4&&8]) {
+//             $('.menu-box-3 ul li:last-child').after('<li>test1</li><li>test1</li>');
+//         } else if ($String == [1&&5&&9]) {
+//             $('.menu-box-3 ul li:last-child').after('<li>test1</li>');
+//         } else if ($String == [0&&2&&6]) {
+//             return;
+//         }
+//     }
+// }
 
 $(function () {
     hasClass__init();
