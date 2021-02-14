d0f493da1790055ffe956031f6fd05119179250d
kkt9102 <kkt9102@gmail.com>
2021-02-14 15:07:29 +0900

    메뉴개수 작동안됨...

--- portfolio/reserve/resource/js/common.js
+++ portfolio/reserve/resource/js/common.js
@@ -24,6 +24,40 @@ function hasClass__init() {
         $(this).closest('li').removeClass('active');
     });
 }
+// section-4에 아이템 개수정렬
+function Coffee_item() {
+        var $Item = $('#section-4 .section-4_con .menu-box-3 ul li:last-child');
+        var $Item_index = $('#section-4 .section-4_con .menu-box-3 ul li').length;
+        var $String = $Item_index.toString()[1];
+    
+    
+        if ($Item_index.toString().length == 2 ){
+            if ($String == [3 && 7]) {
+                $Item.append('<li>test1</li><li>test1</li><li>test1</li>');
+                alert('3');
+            } else if ($String == [4 && 8]) {
+                $Item.append('<li>test1</li><li>test1</li>');
+                alert('2');
+            } else if ($String == [1 && 5 && 9]) {
+                $Item.append('<li>test1</li>');
+                alert('1');
+            } else if ($String == [0 && 2 && 6]) {
+                return;
+            }
+        } else {
+            if ($String == [3&&7]) {
+                $Item.append('<li>test1</li><li>test1</li><li>test1</li>');
+            } else if ($String == [4&&8]) {
+                $Item.append('<li>test1</li><li>test1</li>');
+            } else if ($String == [1&&5&&9]) {
+                $Item.append('<li>test1</li>');
+            } else if ($String == [0&&2&&6]) {
+                return;
+            }
+        };
+}
+
 $(function () {
     hasClass__init();
+    Coffee_item();
 });
