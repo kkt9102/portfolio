05d43825fc7bc8f5ca8ccf35676d6c070058a0d7
kkt9102 <kkt9102@gmail.com>
2021-02-21 12:19:45 +0900

    포트폴리오 메인 팝업 준비중

--- resource/js/common.js
+++ resource/js/common.js
@@ -1,8 +1,21 @@
-function test(){
-    $('.test').click(function(){
-        alert("test");
+function popup__open() {
+    var $popup_bg = $('.popup-win-list > .popup-bg');
+    $('#section-3 .portfolio-menu > .btn-list > li').click(function(){
+        var index = $(this).index();
+        var $popup_win = $('#section-3 .popup-win-list > .popup-list > li');
+
+            $popup_win.eq(index).addClass('active');
+            $popup_bg.addClass('active');
+    })
+}
+function popup_close() {
+    $('.popup-bg,.popup-close-btn').click(function(){
+        $('.popup-bg').removeClass('active');
+        $('.popup-close-btn')
     });
 }
+
 $(function(){
-    test();
-});
+    popup__open();
+    popup_close();
+})
