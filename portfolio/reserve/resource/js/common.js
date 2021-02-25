88cee3dbd322b570956ae5e7cb6296e8a08bb641
kkt9102 <kkt9102@gmail.com>
2021-02-25 18:11:27 +0900

    리저브 반응형작업

--- portfolio/reserve/resource/js/common.js
+++ portfolio/reserve/resource/js/common.js
@@ -1,4 +1,8 @@
 function hasClass__init() {
+    var index
+    var $popup_win
+    var $popup_Bg
+
     $('#header .bars-btn').click(function () {
         var $menuBox_1 = $('#header > .menu-box-1');
         var $header = $('#header');
@@ -13,19 +17,46 @@ function hasClass__init() {
             $header.addClass('active');
         }
     });
+
     $('.extraction-list ul li .popup-img').click(function () {
-        var $popup_win = $('.extraction-list .popup-list .popup-win');
-        var index = $(this).parent().index();
+        $popup_win = $('.extraction-list .popup-list .popup-win');
+        index = $(this).parent().index();
+        var $popup_bg = $('.extraction-list .popup-bg');
 
         $popup_win.removeClass('active');
+        $popup_bg.addClass('active');
         $popup_win.eq(index).addClass('active');
 
+        var $next_popup = $('.extraction-list .popup-list > .popup-win > .con .right');
+        var $prev_popup = $('.extraction-list .popup-list > .popup-win > .con .left');
+        
+        $next_popup.click(function () {
+            $(this).closest('.popup-win').removeClass('active');
+            $(this).closest('.popup-win').next().addClass('active');
+            if ($(this).closest('.popup-win').next().length == 0){
+                $('.extraction-list .popup-list').find('> .popup-win:first-child').addClass('active');
+            }
+        })
+        $prev_popup.click(function () {
+            $(this).closest('.popup-win').removeClass('active');
+            $(this).closest('.popup-win').prev().addClass('active');
+            if ($(this).closest('.popup-win').prev().length == 0){
+                $('.extraction-list .popup-list').find('> .popup-win:last-child').addClass('active');
+            }
+        })
+
     });
+    
     $('.extraction-list .popup-list > .popup-win > .con >.popup-win-header > .close-btn').click(function(){
+        var $popup_bg = $('.extraction-list .popup-bg')
         $(this).closest('li').removeClass('active');
+        $popup_bg.removeClass('active');
     });
+
+
 }
-/* scroll-guide-btn moving */
+
+// scroll-guide-btn moving
 function guide_arrow() {
     $(document).ready(function(){
         $('#section-1 .guide-arrow .btn img').animate({
